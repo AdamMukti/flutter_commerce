a11b5a8cc665bac3403cbe773ec5734159197210
Adam Mukti <adammukti08@gmail.com>
2023-10-29 18:48:12 +0700

    Add data result to UI

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_commerce/src/views/home_page.dart';
 
 class App extends StatelessWidget {
-  const App({super.key});
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,101 +1,203 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_commerce/src/components/product_card.dart';
+import 'package:flutter_commerce/core/model/product_model.dart';
+import 'package:flutter_commerce/core/service/api_service.dart';
+// import 'package:flutter_commerce/src/components/product_card.dart';
 import 'package:flutter_commerce/src/utility/theme_color.dart';
 import 'package:flutter_commerce/src/utility/theme_text.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late List<ProductModel>? _productModel = [];
+  @override
+  void initState() {
+    super.initState();
+    _getData();
+  }
+
+  void _getData() async {
+    _productModel = (await ApiService().getUsers());
+    // print("TES NIH");
+    Future.delayed(const Duration(seconds: 1)).then((value) => setState(() {}));
+    print(_productModel);
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Text(
-              'Halo, Adam Mukti',
-              style: textHeading,
-            ),
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Center(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 16),
-              height: 60,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: Color(0xFF000000).withOpacity(0.1),
-                    blurRadius: 15,
-                    offset: Offset(0, 4), // changes position of shadow
+      body: _productModel == null || _productModel!.isEmpty
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      'Halo, Adam Mukti',
+                      style: textHeading,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Center(
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 16),
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Color(0xFF000000).withOpacity(0.1),
+                            blurRadius: 15,
+                            offset: Offset(0, 4), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        // onChanged: (value) => _runFilter(value),
+                        // controller: ListMahasiswa.searchBar,
+                        decoration: InputDecoration(
+                          hintText: 'Search',
+                          hintStyle: textMuted.copyWith(fontSize: 14),
+                          filled: true,
+                          fillColor: Colors.white,
+                          prefixIconColor: primary,
+                          prefixIcon: Icon(Icons.search),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Categories',
+                          style: textHeading,
+                        ),
+                        Text('See All'),
+                      ],
+                    ),
+                  ),
+                  // ProductCard(),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: GridView.builder(
+                      itemCount: _productModel!.length,
+                      shrinkWrap: true,
+                      physics: ScrollPhysics(),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: .8,
+                        crossAxisSpacing: 16,
+                        mainAxisSpacing: 16,
+                      ),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Row(
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Expanded(
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(16),
+                                      bottomRight: Radius.circular(16),
+                                      topLeft: Radius.circular(16),
+                                      topRight: Radius.circular(16),
+                                    ),
+                                    child: Image.network(
+                                      _productModel![index].thumbnail,
+                                      // width: 100,
+                                      height: 150,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Flexible(
+                                    child: Text(
+                                      _productModel![index].title,
+                                      overflow: TextOverflow.ellipsis,
+                                      softWrap: true,
+                                      style: textTitle,
+                                    ),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Icon(Icons.star, color: Colors.yellow),
+                                      Text(
+                                        _productModel![index].rating.toString(),
+                                        style: textTitle,
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Text(
+                                    '\$${_productModel![index].price}',
+                                    style: textHeading,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Center(
+                      child: Text('End of result.'),
+                    ),
                   ),
                 ],
               ),
-              child: TextField(
-                // onChanged: (value) => _runFilter(value),
-                // controller: ListMahasiswa.searchBar,
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  hintStyle: textMuted.copyWith(fontSize: 14),
-                  filled: true,
-                  fillColor: Colors.white,
-                  prefixIconColor: primary,
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-              ),
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Categories',
-                  style: textHeading,
-                ),
-                Text('See All'),
-              ],
-            ),
-          ),
-          // ProductCard(),
-          SizedBox(
-            height: 16,
-          ),
-          Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Expanded(
-                child: GridView.builder(
-                  itemCount: 4,
-                  shrinkWrap: true,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    childAspectRatio: .8,
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                  ),
-                  itemBuilder: (context, index) => const ProductCard(),
-                ),
-              ),
-            )
-        ],
-      ),
     ));
   }
 }
