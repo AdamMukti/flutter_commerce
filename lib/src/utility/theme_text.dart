763d4e7f47b0d6ab4fe20d843282e91be99edeb5
Adam Mukti <adammukti08@gmail.com>
2023-10-29 10:35:09 +0700

    Add color and text utility

---
+++ lib/src/utility/theme_text.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_commerce/src/utility/theme_color.dart';
+
+TextStyle textHeading = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: dark,
+);
+TextStyle textTitle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  color: dark,
+);
+TextStyle textParagraph = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  color: dark,
+);
+TextStyle textMuted = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  color: secondary,
+);
