623b33fe1c81f4a4c1337acc89e76bc988d76c98
Adam Mukti <adammukti08@gmail.com>
2023-10-29 10:39:11 +0700

    Add search bar

--- lib/src/utility/theme_color.dart
+++ lib/src/utility/theme_color.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 Color white = Color(0xFFFFFFFF);
-Color primary = Color(0xFF51C3FF);
+Color primary = Color(0xFF19C463);
 Color dark = Color(0xFF1A1A2E);
 Color secondary = Color(0xffACADAE);
-
--- lib/src/views/home_page.dart
+++ lib/src/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commerce/src/utility/theme_color.dart';
+import 'package:flutter_commerce/src/utility/theme_text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,6 +10,7 @@ class HomePage extends StatelessWidget {
     return SafeArea(
         child: Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
             height: 40,
@@ -16,7 +19,63 @@ class HomePage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Text(
               'Halo, Adam Mukti',
+              style: textHeading,
             ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16),
+              height: 60,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF000000).withOpacity(0.1),
+                    blurRadius: 15,
+                    offset: Offset(0, 4), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: TextField(
+                // onChanged: (value) => _runFilter(value),
+                // controller: ListMahasiswa.searchBar,
+                decoration: InputDecoration(
+                  hintText: 'Search',
+                  hintStyle: textMuted.copyWith(fontSize: 14),
+                  filled: true,
+                  fillColor: Colors.white,
+                  prefixIconColor: primary,
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Categories',
+                  style: textHeading,
+                ),
+                Text('See All'),
+              ],
+            ),
+          ),
+          // ProductCard(),
+          SizedBox(
+            height: 16,
           ),
         ],
       ),
