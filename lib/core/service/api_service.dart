9a897b9d845a6021f0e2b2ab5da819d4e1fe1c0b
Adam Mukti <adammukti08@gmail.com>
2023-10-29 20:57:48 +0700

    Add api service to get categories

--- lib/core/service/api_service.dart
+++ lib/core/service/api_service.dart
@@ -1,6 +1,7 @@
 // import 'dart:convert';
 import 'dart:convert';
 import 'dart:developer';
+import 'package:flutter_commerce/core/model/categories_model.dart';
 import 'package:flutter_commerce/core/model/product_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -25,9 +26,22 @@ class ApiService {
       var response = await http.get(url);
       if (response.statusCode == 200) {
         List<ProductModel> _model = productModelFromJson(
-        json.encode(json.decode(response.body)['products']));
+            json.encode(json.decode(response.body)['products']));
         return _model;
-        print(url);
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+
+  Future<List<String>?> getCategories() async {
+    try {
+      var url = Uri.parse('https://dummyjson.com/products/categories');
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<String> _model = categoriesModelFromJson(response.body);
+        print(_model);
+        return _model;
       }
     } catch (e) {
       log(e.toString());
